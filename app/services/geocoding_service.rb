f2afc1e49bd830d4ad3ccc1b31898594ee428a73
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-07 11:44:09 -0600

    Add geocoding service

--- app/services/geocoding_service.rb
+++ app/services/geocoding_service.rb
@@ -1,7 +1,21 @@
 class GeocodingService
   class << self
     def get_lat_long(city_params)
-      get_json(conn)
+      response = conn(city_params)
+      get_json(response)
+    end
+
+    private
+
+    def conn(city_params)
+      Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?') do |req|
+        req.params['key'] = ENV['GOOGLE_MAPS_API_KEY']
+        req.params['address'] = city_params
+      end
+    end
+
+    def get_json(response)
+      JSON.parse(response.body, symbolize_names: true)
     end
   end
 end
