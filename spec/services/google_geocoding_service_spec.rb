cda9eb036884aeab3d26fb1e4fbb4cdce044cbce
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-07 12:08:52 -0600

    Update test fixture to new file path

--- spec/services/google_geocoding_service_spec.rb
+++ spec/services/google_geocoding_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Google geocoding service' do
   it 'can return lat and long' do
     VCR.use_cassette('denver_city_search') do
       adress = 'denver,co'
-      json_city_search_resp = File.read('spec/fixtures/city_search.json')
+      json_city_search_resp = File.read('spec/fixtures/test_responses/city_search.json')
       expected = JSON.parse(json_city_search_resp, symbolize_names: true)
       expect(GeocodingService.get_lat_long(adress)).to eq(expected)
     end
