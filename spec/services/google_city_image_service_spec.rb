cfc457a9800b267afcf2694c909bdae91b721781
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-07 13:20:47 -0600

    Build out photo service spec

--- spec/services/google_city_image_service_spec.rb
+++ spec/services/google_city_image_service_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Google City Image Service' do
-  it 'returns the right image' do
+  it 'returns a photo_reeference' do
     VCR.use_cassette('city_place_search_response') do
       adress = 'denver,co'
       response = PhotoService.get_photo_reference(adress)
       expected = "CmRaAAAAOJ6vOt6yOGSPTkM92Tc2AhiovwuLYVwDOTZTF6QL2jyXgPKPh6JPg0b_i25izPBsIOs9biMxStkYr7ds3bj3vfzWkVXV3W-d0-hL0OLAJGKXm-blpQPXBtb0aHTe4Ii8EhDZ_3Mn7Xjbb2mihzwul3zCGhS7JLGFuP_yd1ma5Zx1DUu1Vu9qDg"
-      expect(response[:candidates].first[:photos].first[:photo_reference]).to eq(expected)
+      expect(response).to eq(expected)
+    end
+  end
+
+  it 'returns a photo' do 
+    VCR.use_cassette('city_place_search_response') do
+      adress = 'denver,co'
+      expected =  "https://maps.googleapis.com/maps/api/place/photo?photoreference=CmRaAAAAOJ6vOt6yOGSPTkM92Tc2AhiovwuLYVwDOTZTF6QL2jyXgPKPh6JPg0b_i25izPBsIOs9biMxStkYr7ds3bj3vfzWkVXV3W-d0-hL0OLAJGKXm-blpQPXBtb0aHTe4Ii8EhDZ_3Mn7Xjbb2mihzwul3zCGhS7JLGFuP_yd1ma5Zx1DUu1Vu9qDg&maxwidth=1080&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+      expect(PhotoService.get_photo(adress)).to eq(expected)
     end
   end
 end
