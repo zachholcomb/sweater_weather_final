f6d6b7cab9a0a83c39fccb644a0005c8ad5322f6
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-07 13:20:57 -0600

    Pass spec for photo service

--- app/services/photo_service.rb
+++ app/services/photo_service.rb
@@ -1,23 +1,33 @@
 class PhotoService
   class << self
+    def get_photo(city_param)
+      url = 'https://maps.googleapis.com/maps/api/place/photo?'
+      photo_reference = "photoreference=#{get_photo_reference(city_param)}"
+      maxwidth = '&maxwidth=1080'
+      key = "&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+      url = "#{url}" + "#{photo_reference}" + "#{maxwidth}" + "#{key}"
+    end
+
     def get_photo_reference(city_param)
-      response = conn(city_param)
-      get_json(response)
+      response = conn.get('/maps/api/place/findplacefromtext/json?') do |req|
+        req.params['input'] = city_param
+        req.params['inputtype'] = 'textquery'
+        req.params['fields'] = 'photos'
+      end
+      results = get_json(response)
+      results[:candidates].first[:photos].first[:photo_reference]
     end
 
     private
 
-    def conn(city_params)
-      Faraday.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?') do |req|
+    def conn
+      Faraday.new('https://maps.googleapis.com') do |req|
         req.params['key'] = ENV['GOOGLE_MAPS_API_KEY']
-        req.params['input'] = city_params
-        req.params['inputtype'] = 'textquery'
-        req.params['fields'] = 'photos'
       end
     end
 
     def get_json(response)
-      JSON.parse(response.body, symbolize_names: true)
+      JSON.parse(response.body, symbolize_names: true)  
     end
   end
 end
