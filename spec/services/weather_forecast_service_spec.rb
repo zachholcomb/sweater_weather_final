00fd1ee525c37bc243913c68eb9b598acdaadbbc
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-07 18:59:24 -0600

    Updates forecast spec to use lng

--- spec/services/weather_forecast_service_spec.rb
+++ spec/services/weather_forecast_service_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Forecast Service' do
   it 'can return a forecast for a given city' do
     VCR.use_cassette('denver_forecast') do
-      city_params = { lat: '39.7392358', lon: '-104.990251'}
+      city_params = { lat: '39.7392358', lng: '-104.990251'}
       response = ForecastService.get_forecast(city_params)
       lat = 39.74
       lon = -104.99
