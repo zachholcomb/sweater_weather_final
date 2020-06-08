1a06f6c7f0a22e6d30537d936e49d860521e7b02
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 11:21:45 -0600

    Build out method to get zomato cuisine id

--- app/services/restaurant_search_service.rb
+++ app/services/restaurant_search_service.rb
@@ -2,14 +2,36 @@ class RestaurantSearchService
   class << self
     def return_restaurant_info(search_param, destination)
       location_id = get_location_id(destination)
-      require 'pry'; binding.pry
+      cuisine_id = get_cuisine_id(location_id, search_param)
+      response = conn.get('/api/v2.1/search?') do |req|
+        req.params['entity_id'] = location_id
+        req.params['entity_type'] = 'city'
+        req.params['cuisines'] = cuisine_id
+      end
+      get_json(response)[:restaurants].first
     end
 
     def get_location_id(location_params)
       response = conn.get('/api/v2.1/cities?') do |req|
         req.params['q'] = location_params
       end
-      get_json(response)
+      get_json(response)[:location_suggestions].first[:id]
+    end
+
+    def get_cuisine_id(location_id, search_param)
+      response = conn.get('/api/v2.1/cuisines?') do |req|
+        req.params['city_id'] = location_id
+      end
+      parse_response(get_json(response), search_param)
+    end
+
+
+    def parse_response(results, search_param)
+      results[:cuisines].each do |cuisine_hash|
+        if cuisine_hash[:cuisine][:cuisine_name].downcase == search_param.downcase
+          return cuisine_hash[:cuisine][:cuisine_id]
+        end
+      end
     end
 
     private
