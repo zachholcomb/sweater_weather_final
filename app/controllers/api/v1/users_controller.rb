b7845066cd1b4ae8aab3f8a14d4e068acfd5cad9
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 17:06:49 -0600

    Add helper methods for all user sad paths

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,32 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    render json: UserSerializer.new(User.create(user_params)) 
+    return render json: Error.missing_params, status: 400 if missing_params?
+    return render json: Error.mismatched_passwords, status: 400 if passwords_match?
+    return render json: Error.same_email, status: 400 if email_in_use?
+    render json: UserSerializer.new(User.create(user_params)), status: 201
   end
 
   private
 
   def user_params
-    params.permit(:email, :password)
+    params.permit(:email, :password, :password_confirmation)
+  end
+
+  def input_params
+    params.slice(:email, :password, :password_confirmation)
+  end
+
+  def missing_params?
+    params[:email].nil? || params[:email].empty? || params[:password].nil? ||
+    params[:password].empty? || params[:password_confirmation].nil? ||
+    params[:password_confirmation].empty?
+  end
+
+  def passwords_match?
+    !(params[:password] == params[:password_confirmation])
+  end
+
+  def email_in_use?
+    User.find_by(email: params[:email])
   end
 end
