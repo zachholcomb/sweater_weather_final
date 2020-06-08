e70f8b25759c8559d68c56c4d762db7429c6ba1a
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 15:23:14 -0600

    Adds route and controller action for user post

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :forecast, only: [:index]
       resources :backgrounds, only: [:index]
+      resources :users, only: [:create]
     end
   end
 end
