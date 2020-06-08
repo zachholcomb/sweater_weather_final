f084b75c66b10559b5d4bb877ac18c0254a5c10e
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 17:07:45 -0600

    Add error class for all user

---
+++ app/poros/error.rb
@@ -0,0 +1,28 @@
+class Error
+  attr_reader :id,
+              :message
+  
+  def initialize(message)
+    @id = nil
+    @message = message
+  end
+  class << self
+    def missing_params
+      message = 'Missing required fields'
+      error = Error.new(message)
+      ErrorSerializer.new(error)
+    end
+
+    def mismatched_passwords
+      message = 'Password and pasword confirmation dont match'
+      error = Error.new(message)
+      ErrorSerializer.new(error)
+    end
+
+    def same_email
+      message = 'Email already exists'
+      error = Error.new(message)
+      ErrorSerializer.new(error)
+    end
+  end
+end
