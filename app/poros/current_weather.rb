b4bcf2d07ed74b30c4d8bf7bc00cd5af690c6e81
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-07 18:56:57 -0600

    Adds current_weather poro

---
+++ app/poros/current_weather.rb
@@ -0,0 +1,23 @@
+class CurrentWeather
+  attr_reader :current_temperature,
+              :description,
+              :feels_like,
+              :sunrise,
+              :sunset,
+              :humidity,
+              :uv_index,
+              :temp_min,
+              :temp_max
+
+  def initialize(stats)
+    @current_temperature = stats[:current][:current_temp]
+    @description = stats[:current][:weather][:description]
+    @feels_like = stats[:current][:feels_like]
+    @sunrise = stats[:current][:sunrise]
+    @sunset = stats[:current][:sunset]
+    @humidity = stats[:current][:humidity]
+    @uv_index = stats[:current][:uvi]
+    @temp_min = stats[:daily].first[:temp][:min]
+    @temp_max = stats[:daily].first[:temp][:max]
+  end
+end
