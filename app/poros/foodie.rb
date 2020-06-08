bca0ad7d6e557cf941fe0ab46266500467d458ac
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 09:42:21 -0600

    Adds foodie poro

--- app/poros/foodie.rb
+++ app/poros/foodie.rb
@@ -1,3 +1,11 @@
 class Foodie
-  
+  attr_reader :id,
+              :directions,
+              :restaurant
+
+  def initialize(params)
+    @id = nil
+    @directions = TripInfo.get_trip_info(params[:start], params[:end])
+    @restaurant = Restaurant_Search.new(params[:search])
+  end
 end
