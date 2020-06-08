c80ab309abaa789b75d3f279bb57e07308a2917e
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 11:53:29 -0600

    Updates trip info travel time to the right format

--- app/poros/foodie.rb
+++ app/poros/foodie.rb
@@ -7,5 +7,6 @@ class Foodie
     @id = nil
     @directions = TripInfo.get_trip_info(params[:start], params[:end])
     @restaurant = Restaurant.get_restaurant(params[:search], params[:end])
+    @current_weather = TripWeather.get_destination_weather(params[:end])
   end
 end
--- app/poros/trip_info.rb
+++ app/poros/trip_info.rb
@@ -9,6 +9,7 @@ class TripInfo
 
   def self.get_trip_info(start_param, end_param)
     travel_time = TravelTimeService.time_to_destination(start_param, end_param)
-    TripInfo.new(travel_time, end_param)
+    parsed_time = travel_time[:rows].first[:elements].first[:duration][:text]
+    TripInfo.new(parsed_time, end_param)  
   end
 end
