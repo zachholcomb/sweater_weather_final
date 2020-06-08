05f4cc32098dc2d688c1b6fe694a3ba9637f22fd
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-07 23:46:09 -0600

    Update current weather to format data

--- app/poros/current_weather.rb
+++ app/poros/current_weather.rb
@@ -10,15 +10,28 @@ class CurrentWeather
               :temp_max
 
   def initialize(stats)
-    @current_temperature = stats[:current][:temp]
-    @description = stats[:current][:weather].first[:description]
-    @feels_like = stats[:current][:feels_like]
-    @sunrise = stats[:current][:sunrise]
-    @sunset = stats[:current][:sunset]
+    @current_temperature = stats[:current][:temp].round
+    @description = stats[:current][:weather].first[:main]
+    @feels_like = stats[:current][:feels_like].round
+    @date = format_current_time(stats[:current][:dt])
+    @sunrise = format_clock_time(stats[:current][:sunrise])
+    @sunset = format_clock_time(stats[:current][:sunset])
     @humidity = stats[:current][:humidity]
-    @uv_index = stats[:current][:uvi]
-    @temp_min = stats[:daily].first[:temp][:min]
-    @temp_max = stats[:daily].first[:temp][:max]
-    @icon = stats[:current][:weather].first[:icon]
+    @uv_index = stats[:current][:uvi].round
+    @temp_min = stats[:daily].first[:temp][:min].round
+    @temp_max = stats[:daily].first[:temp][:max].round
+    @icon = get_icon_url(stats[:current][:weather].first[:icon])
+  end
+
+  def format_current_time(time)
+    Time.at(time).strftime('%l:%M %p, %B %e')
+  end
+
+  def format_clock_time(time)
+    Time.at(time).strftime('%l:%M')
+  end
+
+  def get_icon_url(icon)
+    "http://openweathermap.org/img/wn/#{icon}@2x.png"
   end
 end
