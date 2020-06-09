41915a4028913e35873943cd0e300603cd98b712
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 19:06:51 -0600

    Add user relationship to road trip

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password_digest, presence: true
   before_create :set_api_key
+  has_many :road_trips
 
   def set_api_key
     self.api_key = generate_api_key
