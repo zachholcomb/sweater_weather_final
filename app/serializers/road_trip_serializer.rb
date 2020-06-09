32216ea77d79e5749d57b492baefbfbb2325bce1
Zach Holcomb <zachholcombmusic@gmail.com>
2020-06-08 23:52:45 -0600

    Add road trip serializer

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,8 @@
+class RoadTripSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :origin, 
+             :destination,
+             :travel_time,
+             :temp,
+             :weather_summary
+end
